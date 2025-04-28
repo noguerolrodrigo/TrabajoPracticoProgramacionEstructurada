2b5079d7887de679b253f99767be8ba368959cf9
Rodrigo <rodrigo.noguerol@alumnos.frm.utn.edu.ar>
2025-04-27 23:48:50 -0300

    Segundo commit: Ejercicio 02 - Determinar el Mayor de Tres Números

---
+++ src/ejercicios/Ejercicio2.java
@@ -0,0 +1,32 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese el primer número: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Ingrese el segundo número: ");
+        int num2 = scanner.nextInt();
+
+        System.out.print("Ingrese el tercer número: ");
+        int num3 = scanner.nextInt();
+
+        int mayor;
+
+        if (num1 >= num2 && num1 >= num3) {
+            mayor = num1;
+        } else if (num2 >= num1 && num2 >= num3) {
+            mayor = num2;
+        } else {
+            mayor = num3;
+        }
+
+        System.out.println("El mayor es: " + mayor);
+
+        scanner.close();
+    }
+}
